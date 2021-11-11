9655c4ca005e27fa8c95de3564ec339cebd4e218
Bsupinski <66806979+bsupinski@users.noreply.github.com>
2021-11-11 10:10:03 -0500

    switch event to svg

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,15 +1,17 @@
-const codeChoices = document.getElementById("code_choices");
-const codeChoice = document.getElementsByClassName("code_choice")
-const svgCode = document.getElementsByClassName("svg_container")
+const codeChoice = document.querySelectorAll(".code_choice")
+const svgCode = document.querySelectorAll(".code")
 const bG = document.querySelector("body");
 
 
 bG.style.backgroundColor="yellow";
 
 
-codeChoices.addEventListener("click", (e) =>{
-    let target = e.target;
-    if(target === codeChoice[0] || target === svgCode[0]) {
-        bG.style.backgroundColor="blue";
-    }
+svgCode.forEach(choice => {
+    choice.addEventListener("click", (e)=> {
+        target = e.target;
+        console.log(target);
+        if(target === choice[0]){
+            bG.style.backgroundColor="green";
+        }
+    })
 })
